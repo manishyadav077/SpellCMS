923605801bd3f79afe5e83eba2428e08544dc0b6
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-05-19 11:56:41 +0545

    tested with jest

---
+++ frontend/src/__tests__/auth.test.ts
@@ -0,0 +1,10 @@
+// import { userLoginService } from "../services/authService";
+
+// test("login returns user data", async () => {
+//   const response = await userLoginService({
+//     email: "test@example.com",
+//     password: "123456",
+//   });
+//   expect(response).toHaveProperty("token");
+//   expect(response.user.email).toBe("test@example.com");
+// });
